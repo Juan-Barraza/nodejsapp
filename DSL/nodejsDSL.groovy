a179743f6d42dc983fa9bab115e921f6748f9bb2
Juan Barraza <juanbarraza01718@gmail.com>
2026-06-29 10:58:01 -0500

    Update Git repository URL in Jenkins DSL

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,7 +1,7 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
+        git('https://github.com/Juan-Barraza/nodejsapp.git', 'master') { node ->
             node / gitConfigName('Juan-Barraza')
             node / gitConfigEmail('juanbarraza2004@outlook.com')
         }
