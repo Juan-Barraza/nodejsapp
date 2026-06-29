689eeb80851210eb4d5fdb2d3d09724e21ba4aba
Juan Barraza <juanbarraza01718@gmail.com>
2026-06-29 10:59:49 -0500

    Change Docker repository name for nodejs app

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('Juan-Barraza/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
